35e700e7d2b0ac662be9064c514955dbef083898
nikez <nikegreen@yandex.ru>
2023-01-23 21:38:15 +0500

    Модели и связи. Машины и владельцы [#4744] fix sql 2

--- db/scripts/008_ddl_add_column_car_id_in_table_auto_post.sql
+++ db/scripts/008_ddl_add_column_car_id_in_table_auto_post.sql
@@ -1,2 +1,2 @@
-alter table auto_post ADD COLUMN car_id int NOT NULL REFERENCES car(id) DEFAULT 1;
-
+alter table auto_post ADD COLUMN car_id int NOT NULL REFERENCES car(id);
+update auto_post set car_id = 1 where car_id = null;
